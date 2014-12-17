f63f69a98a5242f612ded7d7a8992a9b9a28331d
btcarl <bcarlston@csumb.edu>
2014-12-17 01:28:48 -0500

    Final

--- FinalProject.py
+++ FinalProject.py
@@ -9,6 +9,8 @@ def mainMenu():
         printNow("Type EXIT to exit the program")
         printNow("")
         userinput = requestString("Please Enter Your Choice")
+        if userinput is None:
+          userinput = ""
         userinput = userinput.lower()
         if userinput == 'sound':
             userinput = sound_manipulator()    
@@ -22,36 +24,46 @@ def mainMenu():
             
 def image_manipulator():
     userinput = ""
-	printNow("\nImage Manipulator")
+    printNow("\nImage Manipulator")
     printNow("Please start by selecting the picture you would like to manipulate")
     pictureFile = open_picture()
+    if pictureFile is None:
+        userinput = 'back'
     while (userinput != 'exit' and userinput != 'back'):
         printNow("\nImage Manipulator")
         printNow("Select desired option by entering number")
-        printNow("1. Blend with another picture")
+        printNow("1. Change Picture")
         printNow("2. Cartoonize picture")
-        printNow("3. Change Picture")
+        printNow("3. Blend with another picture")
         printNow("4. Checkerboard the Picture")
-        printNow("5. Add a boarder to the Picture")
-        printNow("6. Save image to a file")
+        printNow("5. Add a border to the Picture")
+        printNow("6. Convert image to four colors")
+        printNow("7. Save image to a file")
         printNow("Type BACK to the main menu")
         printNow("Type EXIT to exit the program")
         userinput= requestString("Please Enter Your Choice")
+        if userinput is None:
+          userinput = ""
         userinput = userinput.lower()
         if userinput == '1':
             pictureFile = open_picture()
         elif userinput == '2':
             pictureFile = cartoonize(pictureFile)
+        elif userinput == '3':
+            printNow("Select picture to blend with")
+            pic2 = open_picture()
+            if pic2 is not None:
+                pictureFile = blend(pictureFile, pic2)
         elif userinput == '4':
             picturefile = coloredCheckerboard(pictureFile)
         elif userinput == '5':
-            boarder = int(input("How big would you like the boarder to be(1 - 100)?"))
-            while (boarder < 0 or boarder > 100):
+            border = int(input("How big would you like the border to be(1 - 100)?"))
+            while (border < 0 or border > 100):
                 printNow("You have entered an invalid number!")
-                boarder = int(input("Please enter a boarder size between 1 and 100"))
+                border = int(input("Please enter a border size between 1 and 100"))
             color = 'unknown'
             while (color == 'unknown'):
-                printNow("What color would you like the boarder to be?")
+                printNow("What color would you like the border to be?")
                 printNow("Enter 1 for BLACK")
                 printNow("Enter 2 for BLUE")
                 printNow("Enter 3 for RED")
@@ -59,20 +71,21 @@ def image_manipulator():
                 printNow("Enter 5 for WHITE")
                 choice = requestString("Enter your selection")
                 if choice == '1':
-                    color = 'black'
+                    color = black
                 elif choice == '2':
-                    color = 'blue'
+                    color = blue
                 elif choice == '3':
-                    color = 'red'
+                    color = red
                 elif choice == '4':
-                    color = 'green'
+                    color = green
                 elif choice == '5':
-                    color = 'white'
+                    color = white
                 else:
                     printNow("Please enter a correct selection")
-            pictureFile = add_boarder(pictureFile, boarder, color)
-            
+            pictureFile = add_border(pictureFile, border, color)
         elif userinput == '6':
+            pictureFile = four_color(pictureFile)
+        elif userinput == '7':
             save_image_file(pictureFile)
         elif userinput == 'exit':
             printNow("Thank you for using the CSIT Guy's Manipulator") 
@@ -87,26 +100,62 @@ def sound_manipulator():
     userinput = ""
     printNow("\nSound Manipulator")
     printNow("Please start by selecting the .wav file you would like to manipulate")
+
     sound_file = open_sound()
+    if sound_file is None:
+        userinput = 'back'
     while (userinput != 'exit' and userinput != 'back'):
         printNow("\nSound Manipulator")
-        
+        printNow("Select desired option by entering number")
+        printNow("1. Open a new sound file")
+        printNow("2. Change the volume variably")
+        printNow("3. Digitze sound")
+        printNow("4. Chipmunk sound")
+        printNow("5. Slow sound")
+        printNow("6. Save Sound to a file")
+        printNow("Type BACK to the main menu")
+        printNow("Type EXIT to exit the program")
+        userinput = requestString("Please Enter Your choice")
+        if userinput == '1':
+            sound_file = open_sound()
+        elif userinput == '2':
+            sound_file = changeVolume(sound_file)
+        elif userinput == '3':
+            sound_file = digitize(sound_file)
+        elif userinput == '4':
+            sound_file = chipmunk(sound_file)
+        elif userinput == '5':
+            sound_file = slowspeech(sound_file)
+        elif userinput == '6':
+            save_sound_file(sound_file)
+        elif userinput == 'exit':
+            printNow("Thank you for using the CSIT Guy's Manipulator") 
+        elif userinput == 'back':
+            userinput = 'back'
+        else:    
+            printNow("Enter a Correct Choice")
     return userinput
 
 def open_picture():
     filename=pickAFile()
-    pic=makePicture(filename)
-    return pic
+    if filename is not None:
+      pic=makePicture(filename)
+      return pic
+    else: 
+      return None
 
 def open_sound():
     filename=pickAFile()
-    sound=makeSound(filename)
-    return sound
-    
+    if filename is not None:
+      sound=makeSound(filename)
+      return sound
+    else: 
+      return None
+ 
 def save_sound_file(sound):
     folder = pickAFolder()
-    userInput= requestString("Enter file name")
-    if (folder is not None) and (userInput is not None):
+    userinput= requestString("Enter file name")
+    if (folder is not None) and (userinput is not None):
         writeSoundTo(sound, (folder + userinput + ".wav" ))
     else:
         printNow("unable to save file")
@@ -120,9 +169,19 @@ def save_image_file(picture):
         printNow("unable to save file")
     
 def blend(pic1, pic2):
-    width, height = getWidth(pic1), getHeight(pic1)
-    blendPic = makeEmptyPicture(getWidth(pic1), getHeight(pic1))
+    width1, height1 = getWidth(pic1), getHeight(pic1)
+    width2, height2 = getWidth(pic2), getHeight(pic2)
     
+    
+    if width1 > width2:
+       width= width2
+    else:
+       width = width1
+    if height1  > height2:
+       height = height2 
+    else:
+       height = height1 
+    blendPic = makeEmptyPicture(width, height)
     for y in xrange(height):
         for x in xrange(width):
             targetPx = getPixel(blendPic, x , y )
@@ -134,6 +193,7 @@ def blend(pic1, pic2):
             
             blendColor = makeColor( (r1 + r2)/2, (g1 + g2)/2, (b1 + b2)/2 )
             setColor(targetPx, blendColor)
+    show(blendPic)
     return blendPic
 
 #Cartoonize
@@ -141,7 +201,7 @@ def blend(pic1, pic2):
 def cartoonize(pic):
     pic = blur(pic, 2, 2)
     pic = colorReducer(pic)
-    explore(pic)
+    repaint(pic)
     return pic
 
 #reduces to 256 colors
@@ -235,6 +295,29 @@ def blur(pic, deviation, radius):
             pixel = getPixel(blurredPicture, x,y)
             setColor(pixel,makeColor(r,g,b))
     return blurredPicture  
+ 
+  
+def four_color(pic):
+  valleyGreen = makeColor(34,139,34)
+  bayBlue = makeColor(25,25,112) 
+  goldenSand = makeColor(189,183,107)
+  reddish = makeColor(139,34,34)
+  pixels = getPixels(pic)
+  for p in pixels: 
+    R = getRed(p)
+    G = getGreen(p)
+    B = getBlue(p)
+    luminance = (0.299*R + 0.587*G + 0.114*B) 
+    if luminance <64:
+      setColor(p, bayBlue)
+    elif luminance >=64 and luminance <128:
+      setColor(p, goldenSand)
+    elif luminance>=128 and luminance <192:
+      setColor(p, valleyGreen)
+    else:
+      setColor(p, reddish)
+  repaint(pic)    
+  return pic  
     
 #Phillip
 #Colored Checkerboard
@@ -249,31 +332,26 @@ def coloredCheckerboard(pic):
       px = getPixel(pic, x, y)
       b = getBlue(px)
       setBlue(px, b * 0)
-  repaint(pic)
   for x in range(width/2, width3):
     for y in range(0, height):
       px = getPixel(pic, x, y)
       g = getGreen(px)
       setGreen(px, g * 0)
-  repaint(pic)
   for x in range(width3, width):
     for y in range(0, height):
       px = getPixel(pic, x, y)
       r = getRed(px)
       setRed(px, r * 0)
-  repaint(pic)
   for x in range(0, width):
     for y in range(0, height/4):
       px = getPixel(pic, x, y)
       r = getRed(px)
       setRed(px, r * 2)
-  repaint(pic)
   for x in range(0, width):
     for y in range(height/4, height/2):
       px = getPixel(pic, x, y)
       r = getBlue(px)
       setBlue(px, r * 2)
-  repaint(pic)
   for x in range(0, width):
     for y in range(height/2, height3):
       px = getPixel(pic, x, y)
@@ -281,7 +359,6 @@ def coloredCheckerboard(pic):
       setGreen(px, b * 2)
       r = getRed(px)
       setRed(px, r * 0)
-  repaint(pic)
   for x in range(0, width):
     for y in range(height3, height):
       px = getPixel(pic, x, y)
@@ -290,21 +367,15 @@ def coloredCheckerboard(pic):
       b = getBlue(px)
       setBlue(px, b * 2)
   repaint(pic)
-  newwidth = width + 50
-  newHeight = height + 50
-  newpic = makeEmptyPicture(newwidth, newHeight)
-  pixels = getPixels(newpic)
-  for p in pixels:
-    setColor(p, black)
-  return (newpic)
+  return pic
   
 #Phillip
-#Add boarder of user choice size and color
-def add_boarder(pic, boardersize, color):
+#Add border of user choice size and color
+def add_border(pic, bordersize, color):
   width = getWidth(pic)
   height = getHeight(pic)
-  neww = width + boardersize
-  newh = height + boardersize
+  neww = width + bordersize
+  newh = height + bordersize
   newpic = makeEmptyPicture(neww, newh)
   pixels = getPixels(newpic)
   for p in pixels:
@@ -312,7 +383,8 @@ def add_boarder(pic, boardersize, color):
   for x in range (0, width):
     for y in range (0, height):
       color=getColor(getPixel(pic, x, y))
-      setColor(getPixel(newpic, x + (boardersize/2), y + (boardersize/2)), color)
+      setColor(getPixel(newpic, x + (bordersize/2), y + (bordersize/2)), color)
+  repaint(pic)
   return newpic
   
   
@@ -320,18 +392,20 @@ def add_boarder(pic, boardersize, color):
 #Double, then divide by two sound
 def changeVolume(sound):
    for sample in range(0, getLength(sound)/4):
-      value = getSampleValue(sample)
-      setSampleValue(sample, value * 2)
+      value = getSampleValueAt(sound, sample)
+      setSampleValueAt(sound, sample, value * 2)
    for sample in range(getLength(sound)/4, getLength(sound)/2):
-      value = getSampleValue(sample)
-      setSampleValue(sample, value * .5)
+      value = getSampleValueAt(sound, sample)
+      setSampleValueAt(sound, sample, value * .5)
    for sample in range(getLength(sound)/2, ((getLength(sound)/2) + (getLength(sound)/4))):
-      value = getSampleValue(sample)
-      setSampleValue(sample, value * 2)
+      value = getSampleValueAt(sound, sample)
+      setSampleValueAt(sound, sample, value * 2)
    for sample in range(((getLength(sound)/2) + (getLength(sound)/4)), getLength(sound)):
-      value = getSampleValue(sample)
-      setSampleValue(sample, value * .5)
-
+      value = getSampleValueAt(sound, sample)
+      setSampleValueAt(sound, sample, value * .5)
+   explore(sound)
+   return sound
+    
 #brian
 #gives sample a digitized sound by taking the average of samples and assigning it to the four
 def digitize(sound):
@@ -344,4 +418,29 @@ def digitize(sound):
       average= sum/4
       for i in range(0,4):
         setSampleValueAt(newSound, sample+i, average)
+  explore(newSound)
   return newSound
+  con
+def chipmunk(sound):
+  samples_count = int(getNumSamples(sound))
+  sample_rate = int(getSamplingRate(sound))
+  highPitch = makeEmptySound(samples_count, int(sample_rate*1.5))
+  index =0
+  for sample in range(0,getLength(sound)):
+    value = getSampleValueAt(sound, sample)
+    setSampleValueAt(highPitch,index,value)
+    index+=1
+  explore(highPitch)    
+  return highPitch
+  
+def slowspeech(sound):
+  samples_count = int(getNumSamples(sound))
+  sample_rate = int(getSamplingRate(sound))
+  slow_sound = makeEmptySound(samples_count, int(sample_rate*.75))
+  index =0
+  for sample in range(0,getLength(sound)):
+    value = getSampleValueAt(sound, sample)
+    setSampleValueAt(slow_sound,index,value)
+    index+=1
+  explore(slow_sound)    
+  return slow_sound  
